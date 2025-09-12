62d148d74936fd6336a6318e09c9e31d7ceafbbb
Eunice Jacob <jacobeunice40@gmail.com>
2025-09-12 20:56:22 +0100

    adding task schema

---
+++ schema/tasks.schema.js
@@ -0,0 +1,27 @@
+const { Schema, model } = require("mongoose");
+
+const taskSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+
+    description: {
+      type: String,
+      required: true,
+      lowercase: true,
+    },
+
+    dateOfCompletion: {
+      default: Date.now(),
+      type: Date,
+      required: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
